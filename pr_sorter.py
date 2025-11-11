98419268bb638d4ff8d103d733e3fc3c424df6d8
Luis Guillot <luis.guillot.lozano@gmail.com>
2025-11-11 18:41:10 +0100

    update assigment 1

---
+++ pr_sorter.py
@@ -0,0 +1,48 @@
+################################################################################
+# Prefix Reversal Sorting
+#
+# Name: YOUR NAME HERE 
+#
+# Please use this comment section to list any references or sources you used
+# to complete the assignment.
+# 
+# List of references:
+#   - 
+# 
+################################################################################
+
+################################################################################
+# Required functions. DO NOT MODIFY THESE FUNCTION DECLARATIONS.
+# Replace the body of each function with your implementation
+################################################################################
+
+# Algorithm for sorting an arbitrary list using prefix reversals.
+# INPUT: arr, a list of integers
+# OUTPUT: a sequence of prefix indices that sort arr
+def pr_general_sort(arr: list[int]) -> list[int]:
+    return []
+
+
+# Algorithm for sorting a tritonic list using prefix reversals.
+# INPUT: arr, a list of integers, assumed to be tritonic
+# OUTPUT: a sequence of prefix indices that sort arr
+def pr_tritonic_sort(arr: list[int]) -> list[int]:
+    return []
+
+# Algorithm for sorting a binary list using prefix reversals.
+# INPUT: arr, a list of binary (0 or 1) values
+# OUTPUT: a sequence of prefix that sort arr
+def pr_binary_sort(arr: list[int]) -> list[int]:
+    return []
+
+# Algorithm for sorting a binary list using prefix reversals.
+# INPUT: arr, a list of ternary (0, 1, or 2) values
+# OUTPUT: a sequence of prefix indices that sort arr
+def pr_ternary_sort(arr: list[int]) -> list[int]:
+    return []
+
+################################################################################
+# HELPER FUNCTIONS
+# put any auxiliary function definitions below here
+################################################################################
+
